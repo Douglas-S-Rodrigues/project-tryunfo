47dedf2f64ec88a782004ad72a5bd70a879135ee
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-03-02 14:36:39 -0300

    add validate for save button

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.validateButton = this.validateButton.bind(this);
+
     this.state = {
       cardName: '',
       cardDescription: '',
@@ -27,7 +29,35 @@ class App extends React.Component {
     this.setState((oldState) => ({
       ...oldState,
       [name]: value,
-    }));
+    }), () => this.validateButton());
+  }
+
+  validateButton = () => {
+    const minForAttr = 0;
+    const maxForAttr = 90;
+    const maxSumAttr = 210;
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+
+    const sumAttr = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+
+    this.setState({
+      isSaveButtonDisabled: !(cardName && cardDescription && cardImage && cardRare
+        && cardAttr1 >= minForAttr
+        && cardAttr1 <= maxForAttr
+        && cardAttr2 >= minForAttr
+        && cardAttr2 <= maxForAttr
+        && cardAttr3 >= minForAttr
+        && cardAttr3 <= maxForAttr
+      && sumAttr <= maxSumAttr),
+    });
   }
 
   render() {
